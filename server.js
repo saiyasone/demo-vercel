55a2c76675ba5a24d2e44228095e550e35e21033
saiyasone <saiyasone595@gmail.com>
2023-09-30 13:29:13 +0700

    deploy all files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send({ message: "Home" }).status(200);
+});
+
+const port = 5000;
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
