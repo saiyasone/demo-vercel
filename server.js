d80b70a9d1d2fa847f5fc1e8ff971088c9f55c21
saiyasone <saiyasone595@gmail.com>
2023-09-30 15:05:41 +0700

    update files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const fs = require("fs");
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,14 @@ app.use(
 
 app.get("/", (req, res) => {
   res.send({ message: "Home" }).status(200);
+});
+app.get("/data", (req, res) => {
+  const path = "./data.json";
+  if (fs.existsSync(path)) {
+    res.send({ message: "Yes" }).status(200);
+  } else {
+    res.send({ message: "No" }).status(400);
+  }
 });
 
 const port = 5000;
